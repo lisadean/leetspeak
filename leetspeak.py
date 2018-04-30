3a92530e48f8fa018c38dbf60487b8aa4716cd7d
Lisa Dean <lisadean@LISAMACPRO2017.local>
2018-04-30 09:38:17 -0400

    first commit

---
+++ leetspeak.py
@@ -0,0 +1,15 @@
+text = "elite speak"
+
+original = "AEGIOST"
+translation = "4361057"
+
+text = text.upper()
+
+new_string = ""
+for c in text:
+    if c in original:
+        new_string = new_string + translation[original.index(c)]
+    else:
+        new_string = new_string + c
+
+print(new_string)
